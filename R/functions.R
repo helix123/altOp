510a17cf9b726898560516204f3167de2d1e2a8b
helix123 <kev_t@gmx.de>
2016-06-27 15:52:32 +0200

    update

--- R/functions.R
+++ R/functions.R
@@ -3,43 +3,84 @@
 
 ##### In einer oder mehrerer Variablen einer formula auf der rechten Seite (RHS) ersetzen, wobei positionsgetreu ersetzt wird #
 #
-#' sub_vars_rhs_one2one - Positionstreue ist ein Feature
+#' sub_vars - Positionstreue ist ein Feature
 #'
-#' @param form formula
+#' @param x formula
 #' @param old zu ersetzende Variablen - stringvector
 #' @param new neue Variablen - stringvector
-#' @return formula aktualisierte Formel
+#' @return formula aktualisierte Formel # Erwähnen: orginal formel wird bewusst nicht zurückgegeben, user kann sie einfach per c(form, res) vorne anhängen]
 #' @export
-sub_vars_rhs_one2one <- function(form, old, new) {
+# TODO: ausführliche examples section, zT aus Vignette nehmen
+sub_vars.formula <- function(x, old, new){
+  # [Todo] Listen als Input nutzbar machen - ja
+  # [Todo] Datastreams (?), Dataframes als Input nutzbar machen - nein
+  # [Todo] andere Formelzeichen außer "+" - ja, aber ggf. anderer Subsititionsmechanismus notwendig (über as.character)
+  # [Todo] option, dass im fall "many to many" nicht gemischt wird, d.h. 3x3-Matrix --> 3 Formeln
+  # [Todo] kompatibilität zu Paketen plm, Formula und lmer - später durch KT, über die S3generics sub_vars.pFormula, sub_vars.Formula etc.
+  # [erledigt] Matrizen, die eigentlich Vektoren sind
+  # [erledigt] old[1] durch zwei neue, old[2] durch 3 neue ersetzen --> Leerfeld in matrix mit "#NULL"
+  # orginal formel wird bewusst nicht zurückgegeben, user kann sie einfach per c(form, res) vorne anhängen
 
-  if (length(old) != length(new)) stop("length(old) != length(new)")
+  functyp <- 0
 
-  lhs <- all.vars(form)[1]
-  rhs <- attr(terms(form, keep.order = TRUE), "term.labels") # Weg über terms() erhält die Interaktionsterme
-
-  for (i in 1:length(old)) {
-    rhs <- gsub(old[i], new[i], rhs)
+  # check input
+  #one 2 one replacement, each variable is replaced by exactly one other variable; n to n
+  #one 2 many replacement, each variable is replaced by one or more variables; 1 to n resp.. n to m*n
+  #many to many replacement, each variable is replaced by one or more variables
+  if (class(old)=="character" & class(new)=="character" & length(old)==length(new)) {
+    functyp <- 1
+  } else if (class(old)=="character" & class(new)=="character" & length(old)==1 & length(new) > 1) {
+    functyp <- 2
+  } else if (class(old)=="character" & length(dim(new))==2 & length(old)==length(new[1,])) {
+    functyp <- 3
   }
 
-  res <-as.formula(paste(lhs, "~", paste(rhs, collapse = "+")), env=environment(form))
-  if (res == form) warning("Resulting formula is the same as input formula.")
+  if (functyp == 0) stop("incorrect input")
+
+
+  #lhs <- all.vars(x)[1]                                     # Only works if there is only one variable at the LHS
+  #rhs <- all.vars(x)[2:length(all.vars(x))]                 # Only works if there is only one variable at the LHS
+  rhs <- attr(terms(x, keep.order = TRUE), "term.labels")    # Weg über terms() erhält die Interaktionsterme --> was meinst du damit? => siehe tests
+  lhs <- all.vars(x)[1:(length(all.vars(x))-length(rhs))]
+  res <- NULL
+
+  # check for all old variables to be included in formula
+  for (i in 1:length(old)){
+    if (!(old[i] %in% rhs)) {
+      warning(paste("WARNING:",old[i],"not present in formula:", deparse(x)))
+    }
+  }
+
+
+
+  if (functyp == 1){
+    for (i in 1:length(old)) {
+      rhs <- gsub(old[i], new[i], rhs)
+    }
+    res <-as.formula(paste(paste(lhs, collapse = "+"), "~", paste(rhs, collapse = "+")), env=environment(x))
+  } else if (functyp == 2){
+    for (i in 1:length(new)) {
+      rhs_e <- gsub(old, new[i], rhs)
+      res <- c(res, as.formula(paste(paste(lhs, collapse = "+"), "~", paste(rhs_e, collapse = "+")), env=environment(x)))
+    }
+  } else if (functyp == 3){
+    # ohne austauschen auch ausgeben? --> option
+    all_new <- do.call(expand.grid, split(new, sort(rep(1:ncol(new), nrow(new)))))  # is there a nicer way?
+    for (i in 1:length(all_new[,1])) {
+      rhs_e <- rhs
+      if ("#NULL" %in% c(t(all_new[i,]))) next()    # #NULL for an empty place in the matrix
+      for (j in 1:length(all_new[1,])) {
+        rhs_e <- gsub(old[j], all_new[i,j], rhs_e)
+      }
+      res <- c(res, as.formula(paste(paste(lhs, collapse = "+"), "~", paste(rhs_e, collapse = "+")), env=environment(x)))
+    }
+  }
+
   return(res)
 }
 
-### ALT: extrahiert die ggf. vorhandenen Interaktionsterme nicht
-# sub_vars <- function(form, old, new) {
-#
-#   if (length(old) != length(new)) stop("length(old) != length(new)")
-#
-#   all.variables <- all.vars(form) # extrahiert die ggf. vorhandenen Interaktionsterme nicht
-#
-#   rhs <- all.variables[2:length(all.variables)]
-#   for (i in 1:length(old)) {
-#     rhs <- gsub(old[i], new[i], rhs)
-#   }
-#
-#   return(as.formula(paste(all.variables[1], "~", paste(rhs, collapse = "+")), env=environment(form)))
-# }
-
-
-
+# Generic
+#' @export
+sub_vars <- function(x, ...) {
+  UseMethod("sub_vars")
+}
--- tests/testthat/test_formula.R
+++ tests/testthat/test_formula.R
@@ -6,9 +6,9 @@
 ##       => package should be able to handle those
 
 # TODO:
-# formB  <- formula(compvar ~ roa + log(emp) + beta + age + tenure)
-# formB2 <- sub_vars_rhs_one2one(formB, "emp", "marketcap")      # inside functions: works => log(marketcap)
-# formB3 <- sub_vars_rhs_one2one(formB, "log(emp)", "marketcap") # substitute function: does not (yet?) work; should it?
+formB  <- formula(compvar ~ roa + log(emp) + beta + age + tenure)
+formB2 <- sub_vars(formB, "emp", "marketcap")      # inside functions: works => log(marketcap)
+formB3 <- sub_vars(formB, "log(emp)", "marketcap") # substitute function: does not (yet?) work; should it?
 
 
 # TODO: Tests for generating all formulae
@@ -19,8 +19,8 @@ data("CEOcomp")
 
 test_that("simple variables are substituted", {
   form  <- formula(compvar ~ roa + emp + beta + age + tenure)
-  res_form2 <- sub_vars_rhs_one2one(form, "roa", "roe")
-  res_form3 <- sub_vars_rhs_one2one(form, "roa", "tsr")
+  res_form2 <- sub_vars(form, "roa", "roe")
+  res_form3 <- sub_vars(form, "roa", "tsr")
 
   expect_equal(res_form2,
                formula(compvar ~ roe + emp + beta + age + tenure))
@@ -32,7 +32,7 @@ test_that("simple variables are substituted", {
 test_that("variables substitution in interaction terms,
           all occurences of tenure are substituted, also in the interaction term", {
   form_int <- formula(compvar ~ roa + emp + beta + tenure + tenure:listed)
-  res_form_int2 <- sub_vars_rhs_one2one(form_int, "tenure", "age")
+  res_form_int2 <- sub_vars(form_int, "tenure", "age")
 
   expect_equal(res_form_int2,
                formula(compvar ~ roa + emp + beta + age + age:listed))
@@ -41,13 +41,13 @@ test_that("variables substitution in interaction terms,
 test_that("warning if no change is detectd (output identical to input)", {
   form  <- formula(compvar ~ roa + emp + beta + age + tenure)
   expect_warning(
-    sub_vars_rhs_one2one(form, "emp", "emp"),
+    sub_vars(form, "emp", "emp"),
     "Resulting formula is the same as input formula.")
 })
 
 test_that("LHS shall be preserved when variable is substituted (on RHS)", {
   form  <- formula(log(compvar) ~ roa + emp + beta + age + tenure)
-  res_form2 <- sub_vars_rhs_one2one(form, "emp", "marketcap")
+  res_form2 <- sub_vars(form, "emp", "marketcap")
 
   expect_equal(res_form2,
                formula(log(compvar) ~ roa + marketcap + beta + age + tenure))
@@ -58,16 +58,16 @@ test_that("LHS shall be preserved when variable is substituted (on RHS)", {
 # form <- formula(compvar ~ 0 + roa + emp + beta + age + tenure)
 #
 # all.vars(form) # does not contain constant
-# attr(terms(form), "intercept") # indicates whether constant is present in a formula
+# attr(terms(form), "intercept") # indicates whether intercept is present in a formula
 
-test_that("Respect removal of constant term", {
+test_that("Respect removal of intercept", {
   form1 <- formula(compvar ~ roa + emp + beta + age + tenure - 1)
   form2 <- formula(compvar ~ roa + emp + beta + age + tenure + 0)
   form3 <- formula(compvar ~ 0 + roa + emp + beta + age + tenure)
 
-  res_form1 <- sub_vars_rhs_one2one(form1, "emp", "marketcap")
-  res_form2 <- sub_vars_rhs_one2one(form2, "emp", "marketcap")
-  res_form3 <- sub_vars_rhs_one2one(form3, "emp", "marketcap")
+  res_form1 <- sub_vars(form1, "emp", "marketcap")
+  res_form2 <- sub_vars(form2, "emp", "marketcap")
+  res_form3 <- sub_vars(form3, "emp", "marketcap")
 
   expect_equal(attr(terms(res_form1), "intercept"), 0)
   expect_equal(attr(terms(res_form2), "intercept"), 0)
